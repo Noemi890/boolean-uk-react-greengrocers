0187e1c1a8aa8728495b57ed89c526eaa730c92d
Noemi890 <94084046+Noemi890@users.noreply.github.com>
2022-07-18 15:02:38 +0100

    standard criteria without components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
+import { useState } from 'react'
 import './styles/reset.css'
 import './styles/index.css'
-
 import initialStoreItems from './store-items'
 
 /*
@@ -14,24 +14,106 @@ Here's what a store item should look like
 What should a cart item look like? 🤔
 */
 
-console.log(initialStoreItems)
-
 export default function App() {
-  // Setup state here...
+
+  const addQuantityToItems = () => {
+    initialStoreItems.forEach(item => {
+      item.quantity = 1
+    })
+    return initialStoreItems
+  }
+
+
+  let myTotal = 0
+
+  const [cart, setCart] = useState([])
+  const [storeItems, setStoreItems] = useState(addQuantityToItems)
+  const [total, setTotal] = useState(myTotal.toFixed(2))
+  console.log('my cart', cart)
+  console.log('my total', total)
+
+  const cartCopy = cart.slice()
+
+  const addItemToCart = (item) => {
+    cartCopy.push(item)
+    getTotal()
+    setCart(cartCopy)
+  }
+
+  const addQuantity = (item) => {
+    const index = cart.indexOf(item)
+    cartCopy[index].quantity++
+    getTotal()
+    setCart(cartCopy)
+  }
+
+  const removeQuantity = (item) => {
+    const index = cart.indexOf(item)
+    const myItemQuantity = cartCopy[index].quantity
+    if (myItemQuantity <= 1) cartCopy.splice(index, 1)
+    else cartCopy[index].quantity--
+    getTotal()
+    setCart(cartCopy)
+  }
+
+  const getTotal = () => {
+      cartCopy.forEach(item => {
+        const itemTotal = item.quantity * item.price
+        console.log('total for each item',itemTotal)
+        console.log('myTotal', myTotal)
+        myTotal += itemTotal
+      })
+      setTotal(myTotal.toFixed(2))
+  }
+
+  
+
+  console.log('my store', storeItems)
+  console.log('my cart', cart)
 
   return (
     <>
       <header id="store">
         <h1>Greengrocers</h1>
         <ul className="item-list store--item-list">
-          {/* Wrtite some code here... */}
+          {storeItems.map((item, index) => {
+            return (
+            <li key={index}>
+              <div className='store--item-icon'>
+                <img src={`../../assets/icons/${item.id}.svg`} alt={item.name}/>
+              </div>
+              <button onClick={() => addItemToCart(item)}>Add to cart</button>
+            </li>
+            )
+          })}
         </ul>
       </header>
       <main id="cart">
         <h2>Your Cart</h2>
         <div className="cart--item-list-container">
           <ul className="item-list cart--item-list">
-            {/* Wrtite some code here... */}
+            {
+              (cart.length > 0) &&
+              cart.map((item, index) => {
+                return (
+                  <li key={index}>
+                    <img 
+                    className='cart--item-icon'
+                    src={`/assets/icons/${item.id}.svg`}
+                    alt={item.name}
+                    />
+                    <p>{item.name}</p>
+                    <button 
+                      className='quantity-btn remove-btn center'
+                      onClick={() => {removeQuantity(item)}}>-</button>
+                    <span className='quantity-text center'>{item.quantity}</span>
+                    <button 
+                      className='quantity-btn add-btn center'
+                      onClick={() => {addQuantity(item)}}>+</button>
+                  </li>
+                )
+              })
+            }
           </ul>
         </div>
         <div className="total-section">
@@ -39,7 +121,7 @@ export default function App() {
             <h3>Total</h3>
           </div>
           <div>
-            <span className="total-number">£0.00</span>
+            <span className="total-number">{`£${total}`}</span>
           </div>
         </div>
       </main>
